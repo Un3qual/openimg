3cf62740fc4b29b0fe6ddc76e4547f6c8d86b549
Un3qual <ryan@un3qual.me>
2017-01-29 19:03:46 -0800

    customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+	devise_for :users
+	as :user do
+		get 'login', to: 'devise/sessions#new'
+		get 'register', to: 'devise/registrations#new'
+		delete 'logout', to: 'devise/sessions#destroy'
+	end
 
-  resources :images, :path => 'i'
+	resources :images, :path => 'i'
 
-  get '/new' => 'images#index'
+	get '/new' => 'images#index'
 
-  root to: 'images#new'
+	root to: 'images#new'
 end
