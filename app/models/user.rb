651501b8f553136e259215395a73af1e33122954
Un3qual <ryan@un3qual.me>
2017-01-29 20:08:27 -0800

    finish devise styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   :rememberable, :trackable, :validatable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
-  :format => { with: /\A[a-zA-Z0-9\_]+\Z/, :message => 'Usernames can only have letters, numbers, and underscores.' }
+  :format => { with: /\A[a-zA-Z0-9\_]+\Z/, :message => 'can only have letters, numbers, and underscores.' }
 
   has_many :images
 
